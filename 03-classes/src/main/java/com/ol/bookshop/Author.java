b783276d5bf40d44e6f9b230f138ff29eff0fab5
Owen Lambert <owen.lambert@informed.com>
2021-10-27 15:39:08 +0100

    Created 03-classes for task

---
+++ 03-classes/src/main/java/com/ol/bookshop/Author.java
@@ -0,0 +1,35 @@
+package com.ol.bookshop;
+
+public class Author {
+    private String name;
+    private Address address;
+
+    public Author(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
---
+++ 03-classes/src/main/java/com/ol/bookshop/Book.java
@@ -0,0 +1,66 @@
+package com.ol.bookshop;
+
+public class Book {
+    private String title;
+    private double price;
+    private Author author;
+    private Publisher publisher;
+    private double salePercentage;
+
+    public Book(String title, double price, Author author, Publisher publisher) {
+        this.title = title;
+        this.price = price;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setSaleDiscount(double salePercentage) {
+        this.salePercentage = salePercentage / 100;
+    }
+
+    public double calculateSalePrice() {
+        return price - (price * salePercentage);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                ", author=" + author +
+                ", publisher=" + publisher +
+                '}';
+    }
+}
