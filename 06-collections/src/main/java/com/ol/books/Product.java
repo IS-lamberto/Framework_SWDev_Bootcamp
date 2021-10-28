fda03704e2ed6219a7eb2ab1e6e7cad8b4385b46
Owen Lambert <owen.lambert@informed.com>
2021-10-28 16:19:18 +0100

    Created for collections task using ArrayList

---
+++ 06-collections/src/main/java/com/ol/books/Product.java
@@ -0,0 +1,46 @@
+package com.ol.books;
+
+public abstract class Product implements Sales {
+    private String title;
+    private double price;
+    private double salePercentage;
+
+    public Product(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setSaleDiscount(double salePercentage) {
+        this.salePercentage = salePercentage / 100;
+    }
+
+    public double calculateSalePrice() {
+        return price - (price * salePercentage);
+    }
+
+    public abstract void printer();
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
